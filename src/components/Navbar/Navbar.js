2d746cacb485bcf80d1ac0636198e6379eb849aa
Abdo <abdopoetry@gmail.com>
2019-02-15 20:30:39 +0200

    First commit: main layout

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Navbar = (props) => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
+};
+
+Navbar.propTypes = {};
+
+export default Navbar;
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const MainPublic = (props) => {
+  return (
+    <div>
+      <h1>Main Public</h1>
+    </div>
+  );
+};
+
+MainPublic.propTypes = {};
+
+export default MainPublic;
