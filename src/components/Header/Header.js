262ba0674589d1f7f79b3f657a4ba99741474dba
Abdo <abdopoetry@gmail.com>
2019-04-05 23:47:37 +0200

    Show loading spinner when loading

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import { connect } from 'react-redux';
 import React, { useEffect } from 'react';
+import ReactLoading from 'react-loading';
 
+import { colors } from '../../assets/style/base';
 import * as AuthActions from '../../store/actions/authActions';
 import Jumbotron from './Jumbotron/Jumbotron';
 import Navbar from './Navbar/Navbar';
 
-const Header = ({ checkSavedUserThenLogin }) => {
+const Header = ({ checkSavedUserThenLogin, isLoading }) => {
   useEffect(() => {
     checkSavedUserThenLogin();
   }, []);
@@ -14,15 +16,31 @@ const Header = ({ checkSavedUserThenLogin }) => {
     <>
       <Jumbotron />
       <Navbar />
+
+      {isLoading &&
+        <div style={{
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)'
+        }}>
+          <ReactLoading type="spin" color={colors.main} height={80} width={80} />
+        </div>
+      }
+
     </>
   );
 };
+
+const mapStateToProps = (state) => ({
+  isLoading: state.auth.isLoading || state.daily.isLoading
+})
 
 const mapDispatchToProps = {
   checkSavedUserThenLogin: AuthActions.checkSavedUserThenLogin
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Header);
